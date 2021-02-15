407e54c27c4e4e71dd46e1d0573c36466755cfe3
kirwa <jkirwa25@gmail.com>
2021-02-15 16:43:25 +0300

    ktor initial setup

---
+++ src/main/kotlin/com/skyz/Application.kt
@@ -0,0 +1,42 @@
+package com.skyz
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.util.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+
+
+    }
+    routing {
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
